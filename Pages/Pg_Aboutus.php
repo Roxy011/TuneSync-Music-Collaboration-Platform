1662f71d984f1fa787112162970aac57979b86fa
Roxy011 <rakeshj.mail1106@gmail.com>
2024-09-11 14:10:22 +0530

    first commit

---
+++ Pages/Pg_Aboutus.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" type="image/x-icon" href="../music/assets/Images/headset.png">
+    <link rel="stylesheet" href="../Styles/About.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TuneSYNC</title>
+</head>
+<body>
+    
+    <div class="top-header">
+        <div class="logo"><img onclick="location.href='../Pages/#######'" src="../assets-1/TuneSYNC-1.png" alt=""></div>
+        <div class="nav">
+            <ul>
+                <li><a href="../Pages/Pg_Home.php">Home</a></li>
+                <li><a href="../Pages/Pg_Artist.php">Artist</a></li>
+                <!-- <li><a href="../Pages/Pg_Collaboration.php">Collaboration</a></li> -->
+                <li><a href="../Pages/Pg_Price.php">Plans</a></li>
+                <li style="border-bottom: hsl(0, 0%, 100%) solid;">About Us</li>
+            </ul>
+        </div>
+        <div class="user">
+            <a href="../login.php">Login</a>
+        </div>
+    </div>
+
+    <div class="main-section">
+        <div class="about-part">
+            <div class="details">
+                <ul>
+                    <li><h5>1.Our Mission:</h5></li>
+                    <li>At TuneSYNC, our mission is to bridge musical boundaries and connect artists, producers, and music enthusiasts from around the world. We believe that collaboration is the heartbeat of creativity, and we&comma;re here to foster a global community where musicians can thrive.</li>
+                    <li><h5>2.What We Do:</h5></li>
+                    <li>Media Platform: TuneSYNC serves as a dynamic media platform where artists can share their passion for music.</li>
+                    <li>Cross-Platform Connections: We enable seamless cross-platform connections for playlists, allowing users to discover and share music effortlessly.</li>
+                    <li>Unlimited Sharing: Whether you&comma;re a budding artist or an established musician, TuneSYNC offers free, unlimited sharing for any type of playlist.</li>
+                    <li><h5>3.Why Choose TuneSYNC:</h5></li>
+                    <li>Global Reach: TuneSYNC transcends geographical boundaries, connecting artists across continents. Collaborate with someone from Tokyo while you&comma;re in New York!</li>
+                    <li>Quality Playlists: Discover curated playlists, collaborate on new tracks, and find inspiration from diverse musical genres.</li>
+                    <li>User-Friendly: Our platform is designed to be user-friendly, making it easy for musicians, producers, and music lovers to navigate and connect.</li>
+                    <li><h5>4.Our Community:</h5></li>
+                    <li>Inclusive Community: TuneSYNC is home to a vibrant community of musicians, producers, and music enthusiasts. Join discussions, share insights, and celebrate music together.</li>
+                    <li>Collaboration Opportunities: Whether you&comma;re seeking a vocalist, a guitarist, or a mixing engineer, TuneSYNC provides a space to find the perfect collaborator for your project.</li>
+                    <li><h5>5.Get Involved:</h5></li>
+                    <li>Sign Up: Join TuneSYNC today! Signing up is free, and you&comma;ll gain access to a world of musical possibilities.</li>
+                    <li>Feedback: We value your feedback! Help us improve by sharing your experiences and suggestions.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="footer">
+        <div class="foot-container">
+            <img src="../assets-1/TuneSYNC-1.png" alt="">
+            <p>This Website as all the copyright&copy; by TuneSYNC.</p>
+            <div class="links">
+                <i class="fa fa-instagram" style="font-size:36px"></i>
+                <i class="fa fa-github" style="font-size:36px"></i>
+                <i class="fa fa-linkedin" style="font-size:36px"></i>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
